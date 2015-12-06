adcd51d1afa79b655126af7a0ee4ce36c3889a36
daviddworsky <dworsky.david@gmail.com>
2015-12-06 17:08:04 -0500

    First draft of extension
    Fixes some stuff

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,13 +1,9 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+$(function() {
+	var url = "http://127.0.0.1:5000/matches?for=" + encodeURIComponent(window.location.href);
+	$.ajax(url, {
+		success: function(data) {
+			$("body").prepend(JSON.stringify(data));
+		}
+	})
+});
 
-      console.log(firstHref);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-);
