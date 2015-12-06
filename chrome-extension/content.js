9939878163b23c19543e46152e290d592841bd2b
Ethan Resnick <ethan.resnick@gmail.com>
2015-12-06 18:00:56 -0500

    Extension: start request earlier
    Before, we weren’t even kicking off the ajax request until ondomready,
    which was slower than necessary. Now, we kick it off asap, but don’t
    insert the result til dom ready.

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,9 +1,11 @@
-$(function() {
-	var url = "http://127.0.0.1:5000/matches?for=" + encodeURIComponent(window.location.href);
-	$.ajax(url, {
-		success: function(data) {
-			$("body").prepend(JSON.stringify(data));
-		}
-	})
-});
+(function() {
+  var url = "http://127.0.0.1:5000/matches?for=" + encodeURIComponent(window.location.href);
+  $.ajax(url, {
+    success: function(data) {
+      $(function() {
+        $("body").prepend(JSON.stringify(data));
+      });
+    }
+  });
+}());
 
