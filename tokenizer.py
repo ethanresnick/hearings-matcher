e1f5c79cd040cefde21ebbef2ddb3cba62ecece8
Ethan Resnick <ethan.resnick@gmail.com>
2015-12-06 19:57:56 -0500

    Tokenizer: handle curly quotes and ampersands

--- tokenizer.py
+++ tokenizer.py
@@ -6,11 +6,14 @@ def tokenize(text):
     processed_text = text.lower()
 
     # Remove punctuation
-    processed_text = re.sub(r'[\.\,\(\)\;\:\#\%\*\"\$\-]+', '', processed_text)
+    processed_text = re.sub(ur'[\.\,\(\)\;\:\#\%\*\"\$\-\u201c\u201d]+', '', processed_text)
 
     # Replace atypical whitespace with a single space
     # Remove all numbers too.
-    processed_text = re.sub(r'[\r\t\n\d]+', ' ', processed_text)
+    processed_text = re.sub(ur'[\r\t\n\d]+', ' ', processed_text)
+
+    # Join ampersand separated words into a single unit
+    processed_text = re.sub(ur' \& ', '&', processed_text)
 
     # Split to a list, filter empty tokens, and return
     return filter(lambda x: x != "", processed_text.split(" "))
